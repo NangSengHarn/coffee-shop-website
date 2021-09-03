be270a66d81525436a8fdf40bc01894402967a27
Nang Seng Harn <nangsengharn@gmail.com>
2021-09-03 16:34:20 +0630

    Home Section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-<h1>home</h1>
+<section class="home" id="home">
+  <div class="content">
+    <h3>fresh coffee in the morning</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima ea porro officiis molestiae, quibusdam similique hic nisi dolorum odio. Sunt nostrum est vero nobis, cupiditate ex totam possimus ad.</p>
+    <button class="btn">get yours now</button>
+  </div>
+</section>
 </template>
 
 <script>
@@ -12,5 +18,28 @@ export default {
 </script>
 
 <style>
+.home {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  background:url()no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+.home .content {
+  max-width: 60rem;
+}
+.home .content h3 {
+  font-size: 6rem;
+  text-transform: uppercase;
+  color: #fff;
+}
+.home .content p {
+  font-size: 2rem;
+  font-weight: lighter;
+  line-height: 1.8;
+  padding: 1rem 0;
+  color: #eee;
+}
 
 </style>
